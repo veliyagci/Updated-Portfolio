8304860bf5e5af7c3ce3b16a3d3c5868bd367c52
Veli Yagci <veliyagci35@gmail.com>
2019-05-14 19:09:50 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+
+
+window.onscroll = function() {myFunction()};
+
+
+  
+
+$(document).ready(function () {
+    
+window.onscroll = function() {myFunction()};
+
+
+function myFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            animateBar()
+        } else {
+          document.getElementById("myP").className = "";
+    }
+}function animateBar(){
+
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},4000);
+	});
+ }
+});
+
